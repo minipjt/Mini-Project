de30f52741ac8e33ea32e6fda94d8bec55f56175
anandurdas11 <anandurdas11@gmail.com>
2022-07-16 20:19:19 -0700

    image added

--- music-recomendation/src/App.js
+++ music-recomendation/src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        Music recomendation system
+        <img src="https://t4.ftcdn.net/jpg/01/06/47/61/360_F_106476142_zMZkkTkhMeq0DIjV20oJI00e3QXLYIGN.jpg" />
       </header>
     </div>
   );
